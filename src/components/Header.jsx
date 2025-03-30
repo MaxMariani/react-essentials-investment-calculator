642f6fff1842da873c30005ae407527a3be113cb
Max Mariani <wmaxmariani@gmail.com>
2025-03-30 09:10:56 +0200

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import logo from "../assets/investment-calculator-logo.png"
+
+function Header() {
+    return (
+        <header id="header">
+            <img 
+                src={logo}
+                alt="Logo showing a money bag" />
+            <h1>React Investment Calculator</h1>
+        </header>
+    );
+}
+
+export default Header;
